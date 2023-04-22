5ec850f5ff9786d5f6fb722b0b3840d9e8523784
sunilmsz <inpb91@gmail.com>
2023-04-22 10:06:32 +0530

    eslint setup done

--- documentation/index.js
+++ documentation/index.js
@@ -1,28 +1,28 @@
-const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerJsdoc = require('swagger-jsdoc');
 
 const getSpecifications = (paths) => {
-   
-    const serverUrl = "http://localhost:8010/"
+
+    const serverUrl = 'http://localhost:8010/';
     const options = {
         failOnErrors: true,
         definition: {
-            openapi: "3.0.0",
+            openapi: '3.0.0',
             info: {
-                title: "Backend Coding Test Doc",
-                version: "1.0.0",
+                title: 'Backend Coding Test Doc',
+                version: '1.0.0',
             },
             servers: [
                 {
                     url: serverUrl,
-                    description: "Backend Coding API ",
+                    description: 'Backend Coding API ',
                 },
             ],
             components: {
                 securitySchemes: {
                     bearerAuthToken: {
-                        type: "http",
-                        scheme: "bearer",
-                        bearerFormat: "JWT",
+                        type: 'http',
+                        scheme: 'bearer',
+                        bearerFormat: 'JWT',
                     },
                 },
             },
@@ -30,10 +30,10 @@ const getSpecifications = (paths) => {
         apis: [...paths],
     };
     const openapiSpecification = swaggerJsdoc(options); // openApi creation from yaml or json code
-    
+
     return openapiSpecification;
-}
+};
 
 
-module.exports = { getSpecifications }
+module.exports = { getSpecifications };
 
--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const port = 8010;
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
-const swaggerUI = require("swagger-ui-express");
-const {getSpecifications} = require("./documentation/index")
-const openapiSpecification = getSpecifications(["./documentation/*.yaml"])
+const swaggerUI = require('swagger-ui-express');
+const { getSpecifications } = require('./documentation/index');
+const openapiSpecification = getSpecifications(['./documentation/*.yaml']);
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
@@ -22,8 +22,8 @@ db.serialize(() => {
     const app = require('./src/app')(db);
 
     //swagger documentation
-    app.use("/docs", swaggerUI.serve, swaggerUI.setup(openapiSpecification)); // postman to openApi
-    app.get("/docs.json", (req, res) => {
+    app.use('/docs', swaggerUI.serve, swaggerUI.setup(openapiSpecification)); // postman to openApi
+    app.get('/docs.json', (req, res) => {
         res.send(openapiSpecification);
     });
 
